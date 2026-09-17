63de6f766d658da5719a3d415279e5ff4103ca19
guilherme86711-lang <guilherme86711@gmail.com>
2026-09-17 16:19:02 -0300

    Add files via upload

---
+++ atividade_pontuada3.py
@@ -0,0 +1,18 @@
+import os
+os.system("cls")
+
+#ENTRADA
+
+A = int(input("digite um valor:"))
+B = int(input("digite um valor:"))
+
+#PROCESSO
+
+if A == B:
+    C = A+B
+    print("resultado:", C)
+else:
+    C = A * B
+    print("resultado:", C)
+
+
